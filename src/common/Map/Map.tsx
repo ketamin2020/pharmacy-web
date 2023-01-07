11ddd260eaa2ef8bdeb524f14d12218de31d77fc
Maksym Kovalenko <m.v.kovalenko4@gmail.com>
2023-01-07 12:41:56 +0200

    added contact + fixed map

--- src/common/Map/Map.tsx
+++ src/common/Map/Map.tsx
@@ -2,20 +2,46 @@ import React from 'react'
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import { LatLngTuple } from 'leaflet'
 import { MapContainerProps } from 'react-leaflet'
-const position: LatLngTuple = [50.4138545, 30.5423969]
+import { LocationOn } from '@material-ui/icons'
+import ReactDOMServer from 'react-dom/server'
+import L from 'leaflet'
+import styled from '@emotion/styled'
+import 'leaflet/dist/leaflet.css'
+const MapWrapper = styled.div`
+  & .leaflet-div-icon {
+    border: none;
+    background-color: transparent;
+  }
+`
+
+const position: LatLngTuple = [50.41365970726126, 30.543992123578576]
 const Map: React.FC<MapContainerProps> = props => {
   return (
-    <MapContainer {...props} center={position} zoom={13} scrollWheelZoom={false}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-      />
-      <Marker position={position}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
-    </MapContainer>
+    <MapWrapper>
+      <MapContainer {...props} center={position} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          url='https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png?api_key=4c3b17fc-bee1-42cc-9b17-b4b51480d8c5'
+          attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org">OpenMapTiles</a>, &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+        />
+
+        {/* <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+        /> */}
+
+        <Marker
+          icon={L.divIcon({
+            iconSize: null,
+            html: ReactDOMServer.renderToString(<LocationOn style={{ color: 'red' }} />),
+          })}
+          position={position}
+        >
+          <Popup>
+            49100, Дніпро, пр.Героїв, <br /> б. 11Л, каб. 321
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </MapWrapper>
   )
 }
 
--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
 import styled from '@emotion/styled'
 import Map from 'common/Map/Map'
+import { Phone } from '@material-ui/icons'
+import { EmailOutlined } from '@material-ui/icons'
+import { AccessTime } from '@material-ui/icons'
+import { LocationOn } from '@mui/icons-material'
+import { Phone as PhoneNumber, Email } from 'public'
 const Wrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  gap: 20px;
   & * {
     font-family: var(--primaryFont);
   }
@@ -17,14 +26,93 @@ const Wrapper = styled.div`
     left: -10px;
   }
 `
-const Box = styled.div``
+const Row = styled.div`
+  margin-bottom: 30px;
+  & .title {
+    color: grey;
+    font-size: 13px;
+    display: flex;
+    align-items: center;
+    gap: 2px;
+    margin-bottom: 4px;
+  }
+`
+const Box = styled.div`
+  padding: 20px;
+  border: 1px solid grey;
+  border-radius: 10px;
+`
+
+const items = [
+  { title: 'Директор з маркетингу', phone: PhoneNumber.PHONE_1_PRETY, email: Email.EMAIL_1 },
+  { title: 'Постачальникам', phone: PhoneNumber.PHONE_1_PRETY, email: Email.EMAIL_1 },
+  { title: 'Оптовий продаж', phone: PhoneNumber.PHONE_1_PRETY, email: Email.EMAIL_1 },
+]
 export const Contacts = () => {
   return (
     <Wrapper>
-      <Box>
-        <h3>Клієнтам</h3>
-      </Box>
-      <Map style={{ width: '100%', height: '300px' }} />
+      <>
+        {' '}
+        <Box>
+          <h3>Клієнтам</h3>
+          <Row>
+            <div className='title'>
+              <AccessTime /> Гаряча лінія:
+            </div>
+            <div>
+              <a href={`tel:${PhoneNumber.PHONE_1}`} type='tel'>
+                {PhoneNumber.PHONE_1_PRETY}
+              </a>
+            </div>
+          </Row>
+          <Row>
+            <div className='title'>
+              <Phone /> Графік роботи:
+            </div>
+            <div>з 8:00 до 21:00 (без вихідних)</div>
+          </Row>
+          <Row>
+            <div className='title'>
+              <EmailOutlined /> Служба підтримки:
+            </div>
+            <div>
+              <a href={`mailto:${Email.EMAIL_1}`} type='email'>
+                {Email.EMAIL_1}
+              </a>
+            </div>
+          </Row>
+          <Row>
+            <div className='title'>
+              <LocationOn /> Адреса:
+            </div>
+            <div>49100, Дніпро, пр.Героїв, б. 11Л, каб. 321</div>
+          </Row>
+          <p style={{ width: '300px' }}>
+            Ви можете звернутися за консультацією фармацевта або з будь-якого іншого питання до нас, використовуючи
+            зручний для спосіб.
+          </p>
+        </Box>
+        <Box>
+          {items.map((item, i) => (
+            <Row key={i}>
+              <p>{item.title}</p>
+              <p className='title'>
+                <Phone />
+                <a href={`tel:${item.phone}`} type='tel'>
+                  {item.phone}
+                </a>
+              </p>
+              <p className='title'>
+                <EmailOutlined />
+                <a href={`mailto:${item.email}`} type='email'>
+                  {item.email}
+                </a>
+              </p>
+            </Row>
+          ))}
+        </Box>
+        <Map style={{ width: '300px', height: '400px' }} />
+      </>
     </Wrapper>
   )
 }
